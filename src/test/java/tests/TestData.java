40c32a166939ca66e7585087675f67bd6e36946a
dee7050 <tdk7011@ro.ru>
2021-09-15 23:47:52 +0300

    Add Test with Faker

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,18 +1,9 @@
 package tests;
 
-
-
 public class TestData {
     public static String
-            //firstName = "Sem",
-            //lastName = "Dow",
-            //userEmail = "dow@wu.rt",
-            //userNumber = "8002002020",
-            //subject = "Arts",
-            gender = "Male",
-            hobbies = "Music",
-            state = "NCR",
-            city = "Delhi",
-            picture = "p002.jpg";
-            //currentAddress = "street34";
+        firstName = "Sem",
+        lastName = "Dow",
+        userEmail = "dow@wu.rt",
+        userNumber = "8002002020";
 }
